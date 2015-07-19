d009041b49e51c60afee952569245b6ef82de044
MoeBrowne <oliver.browne@uplyme.com>
2015-07-19 01:47:15 +0100

    Added regex for parameter for a licence

--- git-project-init.sh
+++ git-project-init.sh
@@ -6,3 +6,13 @@ else
 	echo "No project name specified"
 	exit 1
 fi
+
+args=" $@ "
+
+regexArgLicence=' -(-licence|l) ([^ ]+) '
+[[ $args =~ $regexArgLicence ]]
+if [ "${BASH_REMATCH[2]}" != "" ]; then
+	licence="${BASH_REMATCH[2]}"
+else
+	licence=""
+fi
