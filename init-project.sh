ba2b91ef6a219711142720ac09c9e30580bc7c29
MoeBrowne <oliver.browne@uplyme.com>
2015-11-03 21:47:31 +0000

    Added the new description flag to the useage output

--- init-project.sh
+++ init-project.sh
@@ -43,6 +43,9 @@ ${bold}OPTIONS${normal}
 	--name "[NAME]"
 		The name of your project
 
+	--desc "[DESCRIPTION]"
+		A one line description of your project, optional
+
 	--licence [LICENCE]
 		Create licence (MIT, Apache2, GPL2, GPL3), default: no licence
 
